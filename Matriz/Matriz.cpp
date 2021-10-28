be4963858adeb69a3ba74eee15844a74fc52b256
Maria-Sabino <83432324+Maria-Sabino@users.noreply.github.com>
2021-10-27 22:44:29 -0300

    Adicionar arquivos de projeto.

---
+++ Matriz/Matriz.cpp
@@ -0,0 +1,29 @@
+// Matriz.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
+//
+
+public int 2;
+public int 3;
+
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		
+		double[, ] mat = new double[2, 3];
+		
+		Console.WriteLine(mat.Lenght);
+	
+		Console.WriteLine(mat.Rank);
+		 
+		Console.WriteLine(mat.GetLeght(0));
+		
+		Console.WriteLine(mat.GetLeght(1));
+
+		if (Console.ReadLine("Strings")) 
+		{
+			true("")
+		}
+
+	}
+}
